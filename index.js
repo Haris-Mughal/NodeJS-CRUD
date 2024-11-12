eaa24b4cfc9e86f80303c6f01fcb8cf2c274c90f
Haris Mughal <harismirza3456@gmail.com>
2024-11-13 03:09:54 +0500

    db connected and used

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const users = require("./MOCK_DATA.json");
+const mongoose = require("mongoose");
+// const users = require("./MOCK_DATA.json");
 const app = express();
 
 const PORT = 8000;
@@ -9,11 +10,42 @@ const PORT = 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-    req.mw = "Middleware";
-    console.log(`Hello, ${req.mw}!`);
-    next();
-});
+mongoose
+    .connect("mongodb://127.0.0.1:27017/nodejs-crud")
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error("MongoDB Connection Error", err));
+
+const userSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        gender: {
+            type: String,
+        },
+        title: {
+            type: String,
+        },
+    },
+    { timestamps: true }
+);
+
+const User = mongoose.model("user", userSchema);
+
+// app.use((req, res, next) => {
+//     req.mw = "Middleware";
+//     console.log(`Hello, ${req.mw}!`);
+//     next();
+// });
 
 app.use((req, res, next) => {
     fs.appendFile(
@@ -25,10 +57,10 @@ app.use((req, res, next) => {
     );
 });
 
-app.use((req, res, next) => {
-    console.log(`Bye, ${req.mw}!`);
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log(`Bye, ${req.mw}!`);
+//     next();
+// });
 
 const writeToFile = (data, res) => {
     fs.writeFile(
@@ -42,58 +74,106 @@ const writeToFile = (data, res) => {
     );
 };
 
+app.get("/users", async (req, res) => {
+    const allDBUsers = await User.find({});
+
+    const html = `<ul>
+            ${allDBUsers
+                .map((user) => `<li>${user.firstName} - ${user.email}</li>`)
+                .join("")}
+        </ul>`;
+
+    res.send(html);
+});
+
 app.route("/api/users")
-    .get((req, res) => {
-        console.log(req.headers);
-
-        res.setHeader("X-myName", "Haris");
-
-        res.json(users);
+    .get(async (req, res) => {
+        const allDBUsers = await User.find({});
+        res.json(allDBUsers);
     })
-    .post((req, res) => {
-        const newUser = { id: users.length + 1, ...req.body };
-        users.push(newUser);
-        writeToFile(users, res);
-        res.status(201).json({ status: "Success", data: newUser });
+    .post(async (req, res) => {
+        const body = req.body;
+
+        if (
+            !body ||
+            !body.first_name ||
+            !body.last_name ||
+            !body.email ||
+            !body.gender ||
+            !body.title
+        )
+            return res.status(400).json({ message: "All fields are required" });
+
+        const userData = await User.create({
+            firstName: body.first_name,
+            lastName: body.last_name,
+            email: body.email,
+            gender: body.gender,
+            title: body.title,
+        });
+
+        res.status(201).json({
+            msg: "User created successfully",
+            data: userData,
+        });
     })
-    .patch((req, res) => {
+    .patch(async (req, res) => {
         const { id, ...updatedData } = req.body;
-        const userIndex = users.findIndex((user) => user.id === Number(id));
+        const userIndex = User.findById((user) => user.id === id);
 
         if (userIndex !== -1) {
-            users[userIndex] = { ...users[userIndex], ...updatedData };
-            writeToFile(users, res);
+            User[userIndex] = { ...User[userIndex], ...updatedData };
+            writeToFile(User, res);
             res.json({
                 status: "Updated successfully",
-                data: users[userIndex],
+                data: User[userIndex],
             });
         } else {
             res.status(404).json({ status: "User not found" });
         }
     })
-    .delete((req, res) => {
+    .delete(async (req, res) => {
         const { id } = req.body;
-        const userIndex = users.findIndex((user) => user.id === Number(id));
+        const userIndex = User.findById((user) => user.id === id);
 
         if (userIndex !== -1) {
-            const deletedUser = users.splice(userIndex, 1);
-            writeToFile(users, res);
+            const deletedUser = User.splice(userIndex, 1);
+            writeToFile(User, res);
             res.json({ status: "Deleted successfully", data: deletedUser });
         } else {
             res.status(404).json({ status: "User not found" });
         }
     });
 
-app.route("/api/users/:id").get((req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
+app.route("/api/users/:id")
+    .get(async (req, res) => {
+        const user = await User.findById(req.params.id);
 
-    if (user) {
-        res.json(user);
-    } else {
-        res.status(404).json({ status: "User not found" });
-    }
-});
+        if (!user) return res.status(404).json({ status: "User not found" });
+
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).json({ status: "User not found" });
+        }
+    })
+    .patch(async (req, res) => {
+        const user = await User.findByIdAndUpdate(req.params.id, {
+            lastName: "Changed",
+        });
+
+        return res.status(200).json({
+            msg: "Updated successfully",
+            data: user,
+        });
+    })
+    .delete(async (req, res) => {
+        await User.findByIdAndDelete(req.params.id);
+
+        return res.status(200).json({
+            msg: "Deleted successfully",
+        });
+    });
 
 app.listen(PORT, () => {
     console.log(`Server running on PORT: ${PORT}`);
